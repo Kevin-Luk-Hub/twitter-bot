f02c953ad842fe582a55105440a0acebbcf3edcf
Kevin Luk <khl7wh@virginia.edu>
2020-08-15 17:18:30 -0400

    Procfile for deployment

--- credentials.py
+++ credentials.py
@@ -2,10 +2,10 @@ import os
 # App and account details
 # Create developer account at https://developer.twitter.com/en to obtain keys
 
-CONSUMER_KEY = 'WEENNNU22uHDyu8iqjcFsDSPo'
-CONSUMER_SECRET = '4wTPP4Ffg5OtLComF0Y5T44ARxvfXEgk5t1J9b9DJwgAaGHrSo'
-ACCESS_TOKEN = '1284480293416644609-yyjmX6E2Wx2gOiZUa7Inds6nudgIMg'
-ACCESS_TOKEN_SECRET = 'Hs1xNqXmuFp1ywwBGW3Um4uF06Ox0u7AcTMAE5xspMw0f'
+CONSUMER_KEY = os.environ.get('CONSUMER_KEY')
+CONSUMER_SECRET = os.environ.get('CONSUMER_SECRET')
+ACCESS_TOKEN = os.environ.get('ACCESS_TOKEN')
+ACCESS_TOKEN_SECRET = os.environ.get('ACCESS_TOKEN_SECRET')
 
 ACCOUNT_NAME = 'kevDeveloperBot'
 ACCOUNT_ID = '1284480293416644609'
